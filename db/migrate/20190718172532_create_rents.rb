c677bf5c7bcae13699aea2d6ad2dfcefa7155f1b
ALEFALCON <alex.falcon@wolox.com>
2019-07-18 14:27:41 -0300

    Add validation

--- db/migrate/20190718172532_create_rents.rb
+++ db/migrate/20190718172532_create_rents.rb
@@ -1,14 +1,13 @@
 class CreateRents < ActiveRecord::Migration[5.1]
   def change
     create_table :rents do |t|
-      t.integer :user_id, null:false
-      t.integer :book_id, null:false
-      t.date :start_date 
-      t.date :end_date
+      t.references :user_id, null:false
+      t.references :book_id, null:false
+      t.date :start_date, null:false 
+      t.date :end_date, null:false
       t.timestamps
   end
-  
       add_index :rents, :user_id
       add_index :rents, :book_id
-    end
   end
+end
