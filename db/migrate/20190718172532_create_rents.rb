d4640a3d0ddfa635a1cce9e988aee5c04f6864c6
ALEFALCON <alex.falcon@wolox.com>
2019-07-18 14:38:55 -0300

    Fixed model rent

--- db/migrate/20190718172532_create_rents.rb
+++ db/migrate/20190718172532_create_rents.rb
@@ -1,8 +1,8 @@
 class CreateRents < ActiveRecord::Migration[5.1]
   def change
     create_table :rents do |t|
-      t.references :user_id, null: false
-      t.references :book_id, null: false
+      t.integer :user_id, null: false
+      t.integer :book_id, null: false
       t.date :start_date, null: false 
       t.date :end_date, null: false
       t.timestamps
