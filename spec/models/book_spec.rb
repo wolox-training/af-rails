33c4391a795a0c35832451cc0726414318a9bc23
ALEFALCON <alex.falcon@wolox.com>
2019-07-18 10:29:12 -0300

    correction by rubocop and travis

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Book do
   subject(:book) { build(:book) }
-  
+
   it { is_expected.to be_valid }
   it { is_expected.to validate_presence_of(:gender) }
   it { is_expected.to validate_presence_of(:author) }
@@ -10,5 +10,4 @@ describe Book do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:editor) }
   it { is_expected.to validate_presence_of(:year) }
-
 end
