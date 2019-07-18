9ceeac036e7f59aac142acf87b8e5702e337c501
ALEFALCON <alex.falcon@wolox.com>
2019-07-18 14:20:28 -0300

    Replaced syntax by references

--- db/migrate/20190718162511_create_rents.rb
+++ db/migrate/20190718162511_create_rents.rb
@@ -3,13 +3,12 @@ class CreateRents < ActiveRecord::Migration[5.1]
     create_table :rents do |t|
       t.integer :user_id, null:false
       t.integer :book_id, null:false
-      t.date  :start_date 
-      t.date  :end_date
-      
+      t.date :start_date 
+      t.date :end_date
       t.timestamps
-    end
-
-    add_index :rents, :user_id
-    add_index :rents, :book_id
   end
-end
+  
+      add_index :rents, :user_id
+      add_index :rents, :book_id
+    end
+  end
