0c73315d8307ee3c360938babce1536dc42958aa
ALEFALCON <alex.falcon@wolox.com>
2019-07-19 14:55:48 -0300

    Correction by comments

--- db/migrate/20190718185427_create_rents.rb
+++ db/migrate/20190718185427_create_rents.rb
@@ -1,10 +1,10 @@
 class CreateRents < ActiveRecord::Migration[5.1]
   def change
     create_table :rents do |t|
-      t.references :user, foreign_key: true
-      t.references :book, foreign_key: true
-      t.date :start_date
-      t.date :end_date
+      t.references :user, foreign_key: true, null: false
+      t.references :book, foreign_key: true, null: false
+      t.date :start_date, null: false
+      t.date :end_date, null: false
 
       t.timestamps
     end
