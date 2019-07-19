ae0c9220a7c21a6bb97dafe166ca5b7682808403
AleFalcon <52508574+AleFalcon@users.noreply.github.com>
2019-07-19 15:18:20 -0300

    Corrections

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-<<<<<<< HEAD
     password { Faker::Internet.password }
-=======
-    password { Faker::Internet.passwor }
->>>>>>> ced405fc982e7eb8894c6a80653b616a490edb68
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
   end
