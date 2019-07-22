6fa40dd02ca7d69425a0c5f70014fc0c686b051b
ALEFALCON <alex.falcon@wolox.com>
2019-07-22 11:50:22 -0300

    Test Rent model

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,6 +1,5 @@
 class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  validates :user_id, :book_id, presence: true
-  validates :start_date, :end_date, presence: true
+  validates :user_id, :book_id, :start_date, :end_date, presence: true
 end
