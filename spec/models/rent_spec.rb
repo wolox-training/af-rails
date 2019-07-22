1e7c24bca4b247c4e17a3cf758704719358834ec
AleFalcon <52508574+AleFalcon@users.noreply.github.com>
2019-07-22 16:29:43 -0300

    Fixed

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Rent do
-  subject(:rent) { create(:rent) }
+  subject(:rent) { build(:rent) }
 
   it { is_expected.to be_valid }
   it { is_expected.to belong_to(:user) }
