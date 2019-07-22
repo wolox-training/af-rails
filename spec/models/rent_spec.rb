5fec6309c876c8cc9702cda5cb15526155e30feb
ALEFALCON <alex.falcon@wolox.com>
2019-07-22 11:50:22 -0300

    Test Rent model

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Rent do
   subject(:rent) { build(:rent) }
 
-#  it { is_expected.to be_valid }
-#  it { is_expected.to validate_presence_of(:user) }
-#  it { is_expected.to validate_presence_of(:book) }
+  it { is_expected.to_not be_valid }
+  it { is_expected.to_not validate_presence_of(:user) }
+  it { is_expected.to_not validate_presence_of(:book) }
   it { is_expected.to validate_presence_of(:start_date) }
   it { is_expected.to validate_presence_of(:end_date) }
 end
