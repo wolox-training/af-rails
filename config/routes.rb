46411e9c6c1e4d97a6774c3aac7c75d0e7107a56
ALEFALCON <alex.falcon@wolox.com>
2019-07-23 15:44:23 -0300

    Fixed by coments

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   
-  resources :book, only: [:index, :show] do
-    resources :products, only: [:index, :show]
-  end
+  resources :book, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
