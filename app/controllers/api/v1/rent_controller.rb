3f5603490025d66b45cd0e01050019e7d6b3f46b
ALEFALCON <alex.falcon@wolox.com>
2019-07-25 14:51:43 -0300

    Modified rent endpoint

--- app/controllers/api/v1/rent_controller.rb
+++ app/controllers/api/v1/rent_controller.rb
@@ -1,31 +1,30 @@
 module Api
-    module V1
-      class RentController < ApiController
-        include Wor::Paginate
+  module V1
+    class RentController < ApiController
+      include Wor::Paginate
   
-        before_action :authenticate_user!
+      #before_action :authenticate_user!
         
-        @new_user
+      @new_user
 
-        def index
-          render_paginated Rent, each_serializer: RentSerializer
-        end
-  
-        def create
-          @rent = Rent.new(rent_params)
-          if @rent.save
-            render json: @rent, status: :created
-          else
-            render json: @rent.errors, status: :unprocessable_entity
-          end
-        end
-  
-        private
-          def rent_params
-            params.require(:user_id, :book_id, :start_date, :end_date)
-          end
-
+      def index
+        render_paginated Rent, each_serializer: RentSerializer
       end
+  
+      def create
+        @rent = Rent.new(rent_params)
+        if @rent.save
+          render json: @rent, status: :created
+        else
+          render json: @rent.errors, status: :unprocessable_entity
+        end
+      end
+  
+      private
+        def rent_params
+          params.require(:user_id, :book_id, :start_date, :end_date)
+        end
     end
   end
+end
   
