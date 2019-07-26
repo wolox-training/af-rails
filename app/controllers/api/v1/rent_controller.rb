b47238a4251dcbb7ca09dc395cad87044bd972e5
AleFalcon <alex.falcon@wolox.com.ar>
2019-07-26 14:09:18 -0300

    Fixed by rubocop

--- app/controllers/api/v1/rent_controller.rb
+++ app/controllers/api/v1/rent_controller.rb
@@ -2,17 +2,14 @@ module Api
   module V1
     class RentController < ApiController
       include Wor::Paginate
-  
+
       before_action :authenticate_user!
-        
-      @new_user
 
       def index
         render_paginated Rent, each_serializer: RentSerializer
       end
-  
+
       def create
-        byebug
         @rent = Rent.new(current_user, rent_params)
         if @rent.save
           render json: @rent, status: :created
@@ -20,12 +17,12 @@ module Api
           render json: @rent.errors, status: :unprocessable_entity
         end
       end
-  
+
       private
-        def rent_params
-          params.permit(:book_id, :start_date, :end_date)
-        end
+
+      def rent_params
+        params.permit(:book_id, :start_date, :end_date)
+      end
     end
   end
 end
-  
--- app/serializers/rent_serializer.rb
+++ app/serializers/rent_serializer.rb
@@ -1,3 +1,3 @@
 class RentSerializer < ActiveModel::Serializer
-    attributes :user_id,:book_id, :start_date, :end_date
-  end
+  attributes :user_id, :book_id, :start_date, :end_date
+end
