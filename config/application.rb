c3128cd50d5420b13100efacf87b2f6d2291c238
AleFalcon <alex.falcon@wolox.com.ar>
2019-07-30 18:17:42 -0300

    Mail Configuration

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module WbooksApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.active_job.queue_adapter = :sidekiq
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :de
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
