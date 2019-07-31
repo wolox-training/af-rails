f20c1e56d4c1306e60883f55c42fd4387f8440fc
AleFalcon <alex.falcon@wolox.com.ar>
2019-07-31 15:44:42 -0300

    Configuration mail internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   protect_from_forgery with: :null_session
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module WbooksApi
     config.load_defaults 5.1
     config.active_job.queue_adapter = :sidekiq
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
