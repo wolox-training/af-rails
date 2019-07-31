e8e07e495f6560eb2ba3f01ae0d69bbf2f7b85db
AleFalcon <alex.falcon@wolox.com.ar>
2019-07-31 14:38:32 -0300

    Fixed by Rubocop

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -3,7 +3,10 @@ class RentMailer < ApplicationMailer
     email = User.find(rent.user_id).email
     book = Book.find(rent.book_id)
 
-    @message = {"creation_date" => rent.created_at, "book_name" => book.title, "book_author" => book.author, "book_gender" => book.gender, "book_editor" => book.editor, "book_year" => book.year, "start_date" => rent.start_date, "end_date" => rent.end_date}
+    @message = { 'creation_date' => rent.created_at, 'book_name' => book.title,
+                 'book_author' => book.author, 'book_gender' => book.gender,
+                 'book_editor' => book.editor, 'book_year' => book.year,
+                 'start_date' => rent.start_date, 'end_date' => rent.end_date }
 
     mail to: email
   end
