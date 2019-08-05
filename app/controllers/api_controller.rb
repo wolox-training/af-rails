32231bb5d316a87462a28c45bad4925604fc3927
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-05 15:26:14 -0300

    Modifed traduction and private method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,7 @@ class ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :set_locale
 
+  private
   def set_locale
     I18n.locale = current_user.try(:locale) || I18n.default_locale
   end
