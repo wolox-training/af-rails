7442b9aefd1bf9421bf01bacb27864ecc8bbd248
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-05 13:57:10 -0300

    Modified by comments

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,8 @@
 class ApiController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-
-  before_action :set_locale
   protect_from_forgery with: :null_session
-
-  def set_locale
-    I18n.locale = current_user.try(:locale) || I18n.default_locale
-  end
 end
