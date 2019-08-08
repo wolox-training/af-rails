7328ee9a058966aa448e26d3a9943451832cbe6c
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-08 13:26:34 -0300

    Correction json

--- app/services/open_library/book_search.rb
+++ app/services/open_library/book_search.rb
@@ -4,11 +4,11 @@ module OpenLibrary
       isbn = "ISBN:#{number_isbn}"
       response = get('/api/books', query: { bibkeys: isbn })
       @my_book = {
-        'ISBN' => number_isbn,
-        'TITLE' => response.parsed_response[isbn]['name'],
-        'SUBTITLE' => response.parsed_response[isbn]['subtitle'],
-        'NUMBER_OF_PAGES' => response.parsed_response[isbn]['number_of_pages'],
-        'AUTHORS' => response.parsed_response[isbn]['authors']
+        isbn: number_isbn,
+        title: response.parsed_response[isbn]['name'],
+        subtitle: response.parsed_response[isbn]['subtitle'],
+        number_of_pages: response.parsed_response[isbn]['number_of_pages'],
+        authors: response.parsed_response[isbn]['authors']
       }
     end
   end
