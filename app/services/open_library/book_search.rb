fbb354e0bac1f96d815abacec276353ee6f0740d
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-08 14:20:42 -0300

    non-existent message per book

--- app/services/open_library/book_search.rb
+++ app/services/open_library/book_search.rb
@@ -3,13 +3,17 @@ module OpenLibrary
     def self.execute(isbn_number)
       isbn = "ISBN:#{isbn_number}"
       response = get('/api/books', query: { bibkeys: isbn }).parsed_response[isbn]
-      {
-        isbn: isbn_number,
-        title: response['title'],
-        subtitle: response['subtitle'],
-        number_of_pages: response['number_of_pages'],
-        authors: response['authors']
-      }
+      if response.nil?
+        "El libro no existe"
+      else
+        {
+          isbn: isbn_number,
+          title: response['title'],
+          subtitle: response['subtitle'],
+          number_of_pages: response['number_of_pages'],
+          authors: response['authors']
+        }
+      end
     end
   end
 end
