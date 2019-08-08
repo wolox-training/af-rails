b96e747f7e3df7d9cf54af46c723c0d558eb52ab
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-08 13:17:16 -0300

    Fixed test

--- spec/services/open_library_spec.rb
+++ spec/services/open_library_spec.rb
@@ -4,7 +4,6 @@ describe OpenLibrary::BookSearch,
          type: :services do
 
   it 'makes an external request' do
-    WebMock.disable!
     expect(OpenLibrary::BookSearch.execute('0385472579')['TITLE'] == 'Zen speaks')
       .target == true
   end
