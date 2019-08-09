2f319456b27f40f7d541de6a5b526fb14289f1a6
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-09 15:54:47 -0300

    Add test for services open library

--- spec/support/mock_herlper.rb
+++ spec/support/mock_herlper.rb
@@ -1,9 +1,18 @@
 module MockHelper
-  def mocked_book_request_success
+  def book_request_success
     stub_request(:get, /books/)
       .to_return(
         status: 200,
         body: File.read('./spec/support/fixtures/mocked_book_response_success.json'),
+        headers: { 'Content-Type': 'application/json' }
+      )
+  end
+
+  def book_request_not_found
+    stub_request(:get, /books/)
+      .to_return(
+        status: 200,
+        body: File.read('./spec/support/fixtures/mocked_book_response_not_found.json'),
         headers: { 'Content-Type': 'application/json' }
       )
   end
