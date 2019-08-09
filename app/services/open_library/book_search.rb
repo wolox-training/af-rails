a4bfcdfda55708bf547d48d0c4e7455144b74598
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-09 12:56:56 -0300

    Corrections by comments

--- app/services/open_library/book_search.rb
+++ app/services/open_library/book_search.rb
@@ -4,19 +4,19 @@ module OpenLibrary
       isbn = "ISBN:#{isbn_number}"
       response = get('/api/books', query: { bibkeys: isbn }).parsed_response[isbn]
       if response.nil?
-        raise ::Errors::OpenLibrary::BookNotFound.new,
-              "The book with id #{isbn_number} not found"
+        raise Errors::OpenLibrary::BookNotFound.new,
+          I18n.t('services.open_library.errors.book_not_found', isbn_number: isbn_number)
       end
       create_hash(isbn_number, response['title'], response['subtitle'],
                   response['number_of_pages'], response['authors'])
     end
 
-    def create_hash(isbn_number, title, subtitle, nop, authors)
+    def create_hash(isbn_number, title, subtitle, number_of_pages, authors)
       {
         isbn: isbn_number,
         title: title,
         subtitle: subtitle,
-        number_of_pages: nop,
+        number_of_pages: number_of_pages,
         authors: authors
       }
     end
