a0ae09025be79d886a1cd9f52d92fae4db04c6f2
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-12 14:07:52 -0300

    Correction scope

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,5 @@
 class Book < ApplicationRecord
   has_many :rent, dependent: :destroy
   validates :gender, :author, :image, :title, :editor, :year, presence: true
-  scope :long_title, -> { all.select { |book| book.title.size > 25 } }
-
-  def self.long_title
-    Book.long_title
-  end
+  scope :long_title, -> { where('length(title) > ?', 25) }
 end
