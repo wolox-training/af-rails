42dbef568854284f70c186d4c941e11966f6a2a1
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-13 10:44:05 -0300

    Modification seed.rb, include Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,12 @@ User.delete_all if (User.count != 0)
 Book.delete_all if (Book.count != 0)
 
 user = User.create!(first_name: "alex", last_name: "falcon", email: "alex@hotmail.com", password: "123456789", locale: "es", confirmed_at: Time.zone.now)
-50000.times { Book.create!(gender: "Metafiction", author: "Carlene Prosacco", image: "http://ziemann.net/andrea.cain", title: "Frequent Hearses", editor: "Tate Publishing \u0026 Enterprises", year: "2019-08-13") }
+50000.times { Book.create!(gender: Faker::Book.genre, author: Faker::Book.author,
+ image: Faker::Internet.url, title: Faker::Book.title.truncate(25),
+  editor: Faker::Book.publisher, year: Faker::Date.forward(23)) }
 book = Book.last
-Rent.create(user_id: user.id, book_id: book.id, start_date: Date.today, end_date: Date.today)
-BookSuggestion.create!(user_id: user.id, synopsis: "Una gran sinopsis", price: 15.3, author: "Yo soy", title: "Mi otro yo", link: "www.yoyo.com", editor: "Obveo que yo", year: "2019")
+Rent.create(user_id: user.id, book_id: book.id, start_date: Faker::Date.forward(23), end_date: Faker::Date.forward(23))
+BookSuggestion.create!(user_id: user.id, synopsis: Faker::Internet.url('todosloslibros.com', '/synopsis'),
+ price: Faker::Number.decimal(2).to_f, author: Faker::Book.author,
+  title: Faker::Book.title, link: Faker::Internet.url('todosloslibros.com'),
+   editor: Faker::Book.publisher, year: Faker::Date.forward(23))
