deffc66b447e93aabfb39c260fdd1ac7186980dd
AleFalcon <alex.falcon@wolox.com.ar>
2019-08-14 11:57:15 -0300

    Reorder

--- app/interactors/rent_create.rb
+++ app/interactors/rent_create.rb
@@ -4,8 +4,10 @@ class RentCreate < ApiController
     @rent = Rent.new(context.rent)
     #authorize @rent
     if @rent.save
+      render json: context.result, status: :created
       context.result = @rent
     else
+      render json: context.result.errors, status: :unprocessable_entity
       context.fail!
     end
   end
--- app/interactors/rent_mail_for_creation.rb
+++ app/interactors/rent_mail_for_creation.rb
@@ -2,11 +2,6 @@ class RentMailForCreation < ApiController
   include Interactor
   def call
     byebug
-    if context.success?
-      render json: context.rent, status: :created
-      RentMailer.new_rent(context.rent.id).deliver_later
-    else
-      render json: context.rent.errors, status: :unprocessable_entity
-    end
+    RentMailer.new_rent(context.result.id).deliver_later if context.success?
   end
 end
