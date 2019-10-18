3ccf69328a0dd2fe35e8f8f1a11ff021395d2020
AleFalcon <alex.falcon@wolox.com.ar>
2019-10-18 13:43:06 -0300

    Modifiacion archivo travis

--- script_automation_test.sh
+++ script_automation_test.sh
@@ -1,3 +1,3 @@
 for collection in ./test-postman/Flows/*;
- do newman run "$collection" --environment ./test-postman/Environments/environment_postman.json --reporter-html-export ./test-postman/ --reporters=cli,htmlextra; 
+ do node_modules/.bin/newman run "$collection" --environment ./test-postman/Environments/environment_postman.json --reporter-html-export ./test-postman/ --reporters=cli,htmlextra; 
  done
