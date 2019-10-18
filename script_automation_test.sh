3c2c7ede57495bf8292f78be605aa8d8ad89c8a3
AleFalcon <alex.falcon@wolox.com.ar>
2019-10-18 14:25:39 -0300

    Generacion de reportes en html

--- script_automation_test.sh
+++ script_automation_test.sh
@@ -1,3 +1,3 @@
 for collection in ./test-postman/Flows/*;
- do node_modules/.bin/newman run "$collection" --environment ./test-postman/Environments/environment_postman.json --reporter-html-export ./test-postman/; 
+ do node_modules/.bin/newman run "$collection" --environment ./test-postman/Environments/environment_local_postman.json --reporter-html-export ./test-postman/; 
  done
